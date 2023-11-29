808ddedbdebf8c1b90326fa1ad8cfece98212eb7
Agenkia <mh343490@gmail.com>
2023-11-30 01:10:25 +0600

    Some New Method Learned

--- main.java
+++ main.java
@@ -1,6 +1,13 @@
 public class main {
   public static void main(String[] args) {
-    System.out.println("Hi Agenkia");
-    System.out.println("Hello World");
+    System.out.println("Hi Agenkia"); 
+    System.out.println("Hello World"); //println(print line) function
+    System.out.print("This is print Function. ");
+    System.out.print("This is the second line of print function. "); //only print function
+    System.out.println(420);   //Number output
+    System.out.println(4+5);    //Addition
+    System.out.println(4*5); //Multiplication
+    /* How to use multiline comment */ //Multiline comment method
+    
   }
 }
